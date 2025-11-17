c6fbdc5cac451349154a308e727cddfc3c9b08aa
Gergely Kocsis <kocsisger@example.com>
2025-11-17 09:16:07 +0100

    Person entity added

--- src/main/java/en/unideb/inf/bootstrapspring_m8/model/Person.java
+++ src/main/java/en/unideb/inf/bootstrapspring_m8/model/Person.java
@@ -1,4 +1,24 @@
 package en.unideb.inf.bootstrapspring_m8.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "persons")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue
+    Long id;
+    String email;
+    String firstName;
+    String lastName;
+    String password;
+    boolean enabled;
 }
