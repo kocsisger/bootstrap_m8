3d4ea04472e5aa9991e474c9163a4fc92c8390f3
Gergely Kocsis <kocsisger@example.com>
2025-11-24 09:02:16 +0100

    Get persons

--- src/main/java/en/unideb/inf/bootstrapspring_m8/controller/MainController.java
+++ src/main/java/en/unideb/inf/bootstrapspring_m8/controller/MainController.java
@@ -1,17 +1,28 @@
 package en.unideb.inf.bootstrapspring_m8.controller;
 
+import en.unideb.inf.bootstrapspring_m8.model.Person;
+import en.unideb.inf.bootstrapspring_m8.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    PersonRepository personRepository;
+
     @GetMapping("")
     public String showHomepage(){
         return "index";
     }
 
     @GetMapping("/persons")
-    public String getAllPersons(){
+    public String getAllPersons(Model model){
+        List<Person> personsList = personRepository.findAll();
+        model.addAttribute("personsList", personsList);
         return "persons";
     }
 }
