08f9b072c3afeaa08be2397506146c1d4d2446fe
Gergely Kocsis <kocsisger@example.com>
2025-11-24 08:39:11 +0100

    Controller works

--- src/main/java/en/unideb/inf/bootstrapspring_m8/controller/MainController.java
+++ src/main/java/en/unideb/inf/bootstrapspring_m8/controller/MainController.java
@@ -1,7 +1,9 @@
 package en.unideb.inf.bootstrapspring_m8.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
     @GetMapping("")
     public String showHomepage(){
